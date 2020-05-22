00ef3a0c6aaac7d366af684392f08afebcf15cac
seunghyun <ggyool@naver.com>
2020-05-23 00:55:08 +0900

    -

---
+++ scripts/profile.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# 쉬고 있는 profile 찾기: real1 or real2
+
+function find_idle_profile()
+{
+  RESPONE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+
+  if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (4xx,5xx)
+  then
+    CURRENT_PROFILE=real2
+  else
+    CURRENT_PROFILE=$(curl -s http://localhost/profile)
+
+  if [ ${CURRENT_PROFILE} == real1 ]
+  then
+    IDLE_PROFILE=real2
+  else
+    IDLE_PROFILE=real1
+
+    echo "${IDLE_PROFILE}"
+}
+
+
+
+# 쉬고 있는 profile의 port 찾기
+function find_idle_port()
+{
+  IDLE_PROFILE=$(find_idle_profile)
+
+  if [ ${IDLE_PROFILE} == real1 ]
+  then
+    echo "8081"
+  else
+    echo "8082"
+  fi
+}
---
+++ scripts/stop.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+ABSPATH=$(readlink -f $0) # stop.sh 절대경로
+ABSDIR=$(dirname $ABSPATH) # stop.sh 의 상위 폴더
+source=${ABSDIR}/profile.sh # java로 보면 일종의 import (profile.sh 함수를 사용하기 위해)
+
+IDLE_PORT=$(find_idle_port)
+echk "> $IDLE_PORT 에서 구동 중인 애플리케이션 pid 확인"
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT}) # list open file
+
+if [ -z ${IDLE_PID} ]
+then
+  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $IDLE_PID"
+  kill -15 ${IDLE_PID}
+  sleep 5
+fi
+
