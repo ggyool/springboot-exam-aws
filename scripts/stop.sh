9ff4ee4d3341887cf33ec22869e1ac9e440db300
seunghyun <ggyool@naver.com>
2020-05-23 01:40:02 +0900

    -

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-ABSPATH=$(readlink -f $0) # stop.sh 절대경로
-ABSDIR=$(dirname $ABSPATH) # stop.sh 의 상위 폴더
-source=${ABSDIR}/profile.sh # java로 보면 일종의 import (profile.sh 함수를 사용하기 위해)
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
 
 IDLE_PORT=$(find_idle_port)
-echo "> $IDLE_PORT 에서 구동 중인 애플리케이션 pid 확인"
-IDLE_PID=$(lsof -ti tcp:${IDLE_PORT}) # list open file
+
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
 if [ -z ${IDLE_PID} ]
 then
-  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $IDLE_PID"
   kill -15 ${IDLE_PID}
   sleep 5
 fi
-
